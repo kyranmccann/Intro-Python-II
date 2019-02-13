7221f38564ab0d3c038954b437d8d66f8713a8ea
kyranmccann <kyran.mccann@gmail.com>
2019-02-13 10:03:13 -0800

    add basic item class

---
+++ src/item.py
@@ -0,0 +1,10 @@
+#Item class
+
+
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return f'Name: {self.name}, Description: {self.description}'
