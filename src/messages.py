0adfdc45793f3a912154184c24587e00bb5ced2b
kyranmccann <kyran.mccann@gmail.com>
2019-02-14 14:20:59 -0800

    abstract out messages

---
+++ src/messages.py
@@ -0,0 +1,6 @@
+# welcome message
+title = ("\n   Welcome, Student!\n")
+# start intro text
+intro_text = "   As is your custom, you are sitting on your couch re-watching the day's lecture on training kit. It's getting late, and you're finding it hard to stay focused. It\'s not long before your fingers fall away from the keyboard and your eyes fall closed...\n"
+# after acceptance intro text
+intro_cont = "   When you awake, you are no longer curled into your favorite study nook. You are on a beach. Even stranger, you're wearing a ...suit? You fumble around in your pockets and find a wallet. It has a police badge in it. You are apparently a detective. In Australia. Honestly, that's not even the weird part. You can't believe your eyes when you see the date on your identification: 1948.\n"
