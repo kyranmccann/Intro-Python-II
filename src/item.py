e15184b015ba0808d8c328cda10aedc12bcce66d
kyranmccann <kyran.mccann@gmail.com>
2019-02-14 03:50:24 -0800

    add container, update item

--- src/item.py
+++ src/item.py
@@ -1,10 +1,62 @@
-#Item class
+# Item class
 
 
-class Item:
+class Container:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.contents = []
+
+        def on_open(self, player):
+            player.inspecting = [self]
+            print(f'You check the {self.name} and you find  {self.contents}')
+
+        def __repr__(self):
+            return f'Name:{self.name}, description: {self.description}'
+
+
+class Item:
+    def __init__(self, name, description, takable=True, useable=True):
+        self.name = name
+        self.description = description
+        self.take_message = 'You place the ' + name + ' in your briefcase'
+        self.use_message = 'You hold out the ' + name
+        self.takable = takable
+        self.useable = useable
+
+    def on_take_room(self, player):
+        print(f'\n{self.take_message}')
+        player.items.append(self)
+        player.current_room.items.remove(self)
+
+    def on_take_container(self, player, container):
+        print(f'\n{self.take_message}')
+        player.items.append(self)
+        container.contents.remove(self)
+
+    def on_drop(self, player):
+        print(f'\nYou drop the {self.name}')
+        player.location.items.append(self)
+        player.items.remove(self)
+
+    def inspect(self, player):
+        player.inspecting = [self]
+        print(f'\nYou approach the {self.name} to try to get a better look.')
+        print(f'It\'s {self.description}')
+        input('Press enter to continue')
+        player.inspecting = []
+
+    def on_use(self, player):
+        player.using_item = [self]
+        print(f'\n{self.use_message}')
 
     def __repr__(self):
         return f'Name: {self.name}, Description: {self.description}'
+
+
+class Trinket(Item):
+    def __init__(self, color, shape, description):
+        super().__init__("trinket", color, shape, description)
+        self.color = color
+        self.shape = shape
+        self.name = color + description
