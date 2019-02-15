3650109089863d3c61f5f73e0c272eec1c0c8ce1
kyranmccann <kyran.mccann@gmail.com>
2019-02-15 00:54:26 -0800

    clean logic

--- src/item.py
+++ src/item.py
@@ -1,6 +1,6 @@
-# Item class
 
 
+# container class
 class Container:
     def __init__(self, name, description):
         self.name = name
@@ -9,17 +9,19 @@ class Container:
 
     def on_open(self, player):
         player.inspecting_container = [self]
-        print(f'Inspecting the {self.name}, you find')
+        print(f'Inspecting the {self.name}, you find:\n')
         if len(self.contents) > 0:
             for item in self.contents:
-                print(item.name)
+                print(item.name + '\n')
         else:
-            print('nothing more of interest')
+            print('nothing more of interest \n')
+            print('------------------------------------------------------')
 
     def __repr__(self):
         return f'Name:{self.name}, description: {self.description}'
 
 
+# item class
 class Item:
     def __init__(self, name, description, takable=True, useable=True):
         self.name = name
@@ -31,35 +33,32 @@ class Item:
 
     def on_take_room(self, player):
         print(f'\n{self.take_message}')
+        print('------------------------------------------------------')
         player.items.append(self)
         player.current_room.items.remove(self)
 
     def on_take_container(self, player, container):
         print(f'\n{self.take_message}')
+        print('------------------------------------------------------')
         player.items.append(self)
         container.contents.remove(self)
 
     def on_drop(self, player):
         print(f'\nYou drop the {self.name}')
+        print('------------------------------------------------------')
         player.current_room.items.append(self)
         player.items.remove(self)
 
     def inspect(self, player):
         player.inspecting_item = [self]
-        print(f'\nYou approach the {self.name} to try to get a better look.')
-        print(f'It\'s {self.description}')
+        print(f'\nInspecting the {self.name} you see: \n')
+        print(f'{self.description}\n')
+        print('------------------------------------------------------')
 
     def on_use(self, player):
         player.using_item = [self]
         print(f'\n{self.use_message}')
+        print('------------------------------------------------------')
 
     def __repr__(self):
         return f'Name: {self.name}, Description: {self.description}'
-
-
-class Trinket(Item):
-    def __init__(self, color, shape, description):
-        super().__init__("trinket", color, shape, description)
-        self.color = color
-        self.shape = shape
-        self.name = color + description
