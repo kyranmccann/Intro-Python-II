16889b6008700fe1575dd097af13e2b4bc8fb3d4
kyranmccann <kyran.mccann@gmail.com>
2019-02-15 00:54:41 -0800

    update room

--- src/room.py
+++ src/room.py
@@ -1,6 +1,6 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-from item import Item
+import world
 
 
 class Room:
@@ -8,8 +8,26 @@ class Room:
         self.name = name
         self.description = description
         self.return_description = return_description,
+        self.visits = 0
         self.items = []
         self.characters = []
+
+    def inspect(self, player):
+        book = world.item.get('book')
+        print_items = []
+        if self.name == 'Police Station':
+            if [book] in player.items:
+                print_items = self.items
+            else:
+                print_items = [self.items[0]]
+        else:
+            print_items = self.items
+
+        print(f"\nLooking around {self.name}, you see: \n")
+        for item in print_items:
+            print(item.name + '\n')
+        for character in self.characters:
+            print(character.name + '\n')
 
     def __repr__(self):
         return f'Name: {self.name}, description: {self.description}'
