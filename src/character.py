f427d7f3732201bc22fb0852e04a8673bed7c772
kyranmccann <kyran.mccann@gmail.com>
2019-02-15 00:54:19 -0800

    update speak

--- src/character.py
+++ src/character.py
@@ -9,27 +9,35 @@ class Character:
         self.line_two = line_two
         self.required_line = required_line
         self.required_read = False
+        self.required_item = []
         self.item = []
 
     def speak(self, player):
-        if player.using_item == self.required_item and len(self.item) > 0:
-            print(f'\n {self.required_line}')
-            self.required_read = True
-            player.items.append(self.item)
-            self.item = []
+        if len(player.using_item) > 0:
+            if player.using_item[0] == self.required_item and len(self.item) > 0:
+                print(f'\n {self.name}: {self.required_line}')
+                self.required_read = True
+                player.items.append(self.item)
+                print(f'\n{self.name} has nothing more to offer, so you should probably get back to it')
+                input('Press enter to continue')
+                self.item = []
+                player.speaking_to = []
+                player.using_item = []
+
+            elif player.using_item[0] != self.required_item:
+                print(f'{self.name}: {self.line_one}')
+                print('------------------------------------------------------')
+
+        elif len(self.item) == 0:
+            print(f'\n{self.name}: {self.line_two}')
             print(f'\n{self.name} has nothing more to offer, so you should probably get back to it')
             input('Press enter to continue')
             player.speaking_to = []
             player.using_item = []
 
-        elif len(self.item) == 0 or player.using_item != self.required_item:
-            print(f'\n{self.line_two}')
-            print(f'\n{self.name} has nothing more to offer, so you should probably get back to it')
-            input('Press enter to continue')
-            player.speaking_to = []
-            player.using_item = []
         else:
-            print(f'line_one')
+            print(self.line_one)
+            print('------------------------------------------------------')
 
     def first_line(self):
         print(f'\n {self.line_one}')
